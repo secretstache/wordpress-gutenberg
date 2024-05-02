d0e47b5a54dd8dd381b67ab7e9d2b16b82fbbc73
deludd <adimitrov2000@gmail.com>
2024-05-02 13:30:27 +0300

    Configure rollup for scss

--- rollup.config.js
+++ rollup.config.js
@@ -3,6 +3,7 @@ import commonjs from '@rollup/plugin-commonjs';
 import babel from '@rollup/plugin-babel';
 import terser from '@rollup/plugin-terser';
 import external from 'rollup-plugin-peer-deps-external';
+import postcss from 'rollup-plugin-postcss';
 
 const isProd = !process.env.ROLLUP_WATCH;
 
@@ -37,6 +38,11 @@ export default {
             extensions: ['.js', '.jsx'],
         }),
         resolve({ extensions: ['.js', '.jsx'] }),
+        postcss({
+            extract: true,
+            use: ['sass'],
+            extensions: ['.scss'],
+        }),
         isProd && terser(),
     ],
 };
