bbae5d2bafb7ed9198477af6af03c40aa9d92bed
deludd <adimitrov2000@gmail.com>
2024-07-17 15:44:13 +0200

    BCMediaPicker: Add button labels props

--- src/components/MediaPicker.js
+++ src/components/MediaPicker.js
@@ -10,6 +10,8 @@ export const BCImageRenderer = ({
     onImageClick,
     onRemoveClick,
     onSelectClick,
+    selectButtonLabel = "Select Image",
+    removeButtonLabel = "Remove Image",
 }) => {
     return imageId && imageUrl ? (
         <>
@@ -28,7 +30,7 @@ export const BCImageRenderer = ({
                 isSecondary
                 isDestructive
             >
-                Remove Image
+                {removeButtonLabel}
             </Button>
         </>
     ) : (
@@ -37,7 +39,7 @@ export const BCImageRenderer = ({
             onClick={onSelectClick}
             className="bc-select-btn"
         >
-            Select Image
+            {selectButtonLabel}
         </Button>
     );
 };
@@ -47,6 +49,8 @@ export const BCVideoRenderer = ({
     videoUrl,
     onRemoveClick,
     onSelectClick,
+    selectButtonLabel = "Select Video",
+    removeButtonLabel = "Remove Video",
 }) => {
     return videoId && videoUrl ? (
         <>
@@ -60,7 +64,7 @@ export const BCVideoRenderer = ({
                 isSecondary
                 isDestructive
             >
-                Remove Video
+                {removeButtonLabel}
             </Button>
         </>
     ) : (
@@ -69,7 +73,7 @@ export const BCVideoRenderer = ({
             onClick={onSelectClick}
             className="bc-select-btn"
         >
-            Select Video
+            {selectButtonLabel}
         </Button>
     );
 };
@@ -80,6 +84,8 @@ export const BCAnimationRenderer = ({
     animationFileName,
     onSelectClick,
     onRemoveClick,
+    selectButtonLabel = "Select File",
+    removeButtonLabel = "Remove File",
 }) => {
     return animationFileId && animationFileUrl ? (
         <>
@@ -93,24 +99,25 @@ export const BCAnimationRenderer = ({
                 className="bc-remove-btn"
                 onClick={onRemoveClick}
             >
-                Remove File
+                {removeButtonLabel}
             </Button>
         </>
     ) : (
         <Button variant="secondary" onClick={onSelectClick}>
-            Select File
+            {selectButtonLabel}
         </Button>
     )
 };
 
-// TODO: find better name
-export const BCMediaPicker = ({
+export const BCMediaSelector = ({
     mediaId,
     mediaUrl,
     mediaFileName = '',
     onSelect,
     onRemove,
     type = MEDIA_TYPES.IMAGE,
+    selectButtonLabel,
+    removeButtonLabel,
     ...other
 }) => {
     if (type === MEDIA_TYPES.IMAGE) {
@@ -128,6 +135,8 @@ export const BCMediaPicker = ({
                             onImageClick={open}
                             onSelectClick={open}
                             onRemoveClick={onRemove}
+                            selectButtonLabel={selectButtonLabel}
+                            removeButtonLabel={removeButtonLabel}
                         />
                     )}
                     { ...other }
@@ -147,6 +156,8 @@ export const BCMediaPicker = ({
                             videoUrl={mediaUrl}
                             onSelectClick={open}
                             onRemoveClick={onRemove}
+                            selectButtonLabel={selectButtonLabel}
+                            removeButtonLabel={removeButtonLabel}
                         />
                     )}
                     { ...other }
@@ -167,6 +178,8 @@ export const BCMediaPicker = ({
                             animationFileName={mediaFileName}
                             onSelectClick={open}
                             onRemoveClick={onRemove}
+                            selectButtonLabel={selectButtonLabel}
+                            removeButtonLabel={removeButtonLabel}
                         />
                     )}
                     {...other}
