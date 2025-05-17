319af826a0a661af9c059fdb8051ae74c458325d
Dima Fedorenko <dev.fedorenko@gmail.com>
2025-05-17 20:32:24 +0300

    Revert "- ResponsiveSpacingControl: hide input field, add icons"
    This reverts commit 08e1b924eb83753b33a039029cb753375b617d1f.

--- src/components/ResponsiveSpacingControl.js
+++ src/components/ResponsiveSpacingControl.js
@@ -1,7 +1,7 @@
-import { RangeControl, Tooltip } from '@wordpress/components';
 import { TabPanel } from '@wordpress/components';
 import { useCallback, useEffect, useState } from '@wordpress/element';
-import { sidesTop, sidesBottom } from '@wordpress/icons';
+
+import { SpacingControl } from './SpacingControl.js';
 
 const getTabName = (deviceType = 'Desktop') => {
     return deviceType === 'Desktop' ? 'desktop' : 'mobile';
@@ -15,7 +15,7 @@ export const ResponsiveSpacingControl = ({
     onChange,
     value = { desktop: { margin: {}, padding: {} }, mobile: { margin: {}, padding: {} } },
     deviceType,
-}) => {
+ }) => {
     const handleDesktopChange = useCallback(
         (desktop) => {
             onChange({
@@ -88,124 +88,3 @@ export const ResponsiveSpacingControl = ({
         </TabPanel>
     );
 };
-
-export const SpacingControl = ({
-    hasMargin = true,
-    hasPadding = true,
-    disabledMargin = { top: false, bottom: false },
-    disabledPadding = { top: false, bottom: false },
-    marginTooltips = { top: '', bottom: '' },
-    paddingTooltips = { top: '', bottom: '' },
-    max = 6,
-    min = -1,
-    onChange,
-    value = { margin: {}, padding: {} },
-}) => {
-    const handleChange = useCallback(
-        (type, direction) => (newValue) => {
-            onChange({
-                ...value,
-                [type]: {
-                    ...value[type],
-                    [direction]: newValue,
-                },
-            });
-        },
-        [ onChange, value ],
-    );
-
-    return (
-        <>
-            {hasMargin && (
-                <>
-                    <Control
-                        label="Top Margin"
-                        max={max}
-                        min={min}
-                        value={value.margin.top ?? min}
-                        onChange={handleChange('margin', 'top')}
-                        disabled={disabledMargin.top}
-                        tooltip={marginTooltips.top}
-                        afterIcon={sidesTop}
-                    />
-
-                    <Control
-                        label="Bottom Margin"
-                        max={max}
-                        min={min}
-                        value={value.margin.bottom ?? min}
-                        onChange={handleChange('margin', 'bottom')}
-                        disabled={disabledMargin.bottom}
-                        tooltip={marginTooltips.bottom}
-                        afterIcon={sidesBottom}
-                    />
-                </>
-            )}
-
-            {hasPadding && (
-                <>
-                    <Control
-                        label="Top Padding"
-                        max={max}
-                        min={min}
-                        value={value.padding.top ?? min}
-                        onChange={handleChange('padding', 'top')}
-                        disabled={disabledPadding.top}
-                        tooltip={paddingTooltips.top}
-                        afterIcon={sidesTop}
-                    />
-
-                    <Control
-                        label="Bottom Padding"
-                        max={max}
-                        min={min}
-                        value={value.padding.bottom ?? min}
-                        onChange={handleChange('padding', 'bottom')}
-                        disabled={disabledPadding.bottom}
-                        tooltip={paddingTooltips.bottom}
-                        afterIcon={sidesBottom}
-                    />
-                </>
-            )}
-        </>
-    );
-};
-
-const generateMarks = (min, max) => [
-    { value: min, label: min === -1 ? 'Default' : min.toString() },
-    ...Array.from({ length: max - min }, (_, i) => ({
-        value: min + i + 1,
-        label: '',
-    })),
-];
-
-const Control = ({ label, max, min, value, onChange, disabled, tooltip, ...other }) => (
-    <div className="bc-spacing-control-wrapper">
-        <Tooltip
-            text={tooltip}
-            placement="bottom"
-            delay={0}
-        >
-            <div className="bc-spacing-control-content">
-                <RangeControl
-                    className="bc-spacing-range-control"
-                    label={label}
-                    value={value}
-                    disabled={disabled}
-                    onChange={onChange}
-                    min={min}
-                    max={max}
-                    marks={generateMarks(min, max)}
-                    withInputField={false}
-                    resetFallbackValue={-1}
-                    renderTooltipContent={(value) => {
-                        if (value === -1) return 'Default';
-
-                        return value;
-                    }}
-                    {...other}
-                />
-            </div>
-        </Tooltip>
-    </div>
-);
